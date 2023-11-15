6ad579cbc0e154471478704b646804ae2999c1c0
gchandru283 <gchandru283@gmail.com>
2023-11-15 13:41:22 +0530

    commit 2

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/") #decorator
+@app.route("/")  # decorator
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
