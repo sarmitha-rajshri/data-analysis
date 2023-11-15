ed07fef545b1cddcc13d1496a6f2b65cd436a47f
gchandru283 <gchandru283@gmail.com>
2023-11-15 13:26:42 +0530

    Commit 1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,4 +9,7 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
